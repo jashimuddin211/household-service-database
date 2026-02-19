c29f902d6c90d95243509954181089970199bc7f
jashimuddin211 <jashm211@gmail.com>
2026-02-19 12:11:25 +0600

    services data

--- index.js
+++ index.js
@@ -26,6 +26,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+const householdDB = client.db('household')
+const userCollection = householdDB.collection('services')
+
+app.get('/household', async(req, res) =>{
+  const cursor = userCollection.find();
+  const result = await cursor.toArray()
+  res.send(result)
+})
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
